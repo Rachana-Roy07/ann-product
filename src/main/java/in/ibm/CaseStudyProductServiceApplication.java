78e798ea0d5704ce742093f1e50e5dc6666d3f80
Rachana-Roy07 <rachanarr123@gmail.com>
2022-12-28 13:28:22 +0530

    first commit'

---
+++ src/main/java/in/ibm/CaseStudyProductServiceApplication.java
@@ -0,0 +1,13 @@
+package in.ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyProductServiceApplication.class, args);
+	}
+
+}
